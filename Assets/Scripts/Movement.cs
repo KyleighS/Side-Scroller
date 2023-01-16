275227ba9320ca3e5ec67b8062ebb7f02fdc4737
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-01-15 22:29:32 -0500

    animation work

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + (horizontal * Time.deltaTime * speed);
 
