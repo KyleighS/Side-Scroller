7aba333074bf9aef9b3779fb7e44e28bdcd0b497
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-01-16 22:44:00 -0500

    messed around with the jumping and backgrounds

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,20 @@ public class Movement : MonoBehaviour
     public float speed;
     public Animator animator;
 
+    public Rigidbody2D reggiebody;
+    public float jumpForce;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    void Start()
+    {
+        reggiebody = GetComponent<Rigidbody2D>();
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -15,5 +29,9 @@ public class Movement : MonoBehaviour
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + (horizontal * Time.deltaTime * speed);
 
+        if(Input.GetButtonDown("Jump"))
+        {
+            reggiebody.velocity = new Vector2(reggiebody.velocity.x, jumpForce);
+        }
     }
 }
