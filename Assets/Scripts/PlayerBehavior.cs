f5f3f4d6893f03eeb12d07d4724154e149de3135
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-01-21 16:18:19 -0500

    Added Healthbar sprite

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
- 
+    [SerializeField] HealthBar _healthBar;
+    //can just add emeny health hereor in an enemyBehavior script like:
+    //public HealthSystem _playerHealth = new HealthSystem(5,5);
+
     void Start()
     {
         
@@ -30,9 +33,11 @@ public class PlayerBehavior : MonoBehaviour
     private void PlayerTakesDamg(int damg)
     {
         GameManager.gameManager._playerHealth.DamgUnit(damg);
+        _healthBar.SetHealth(GameManager.gameManager._playerHealth.Health);
     }
     private void PlayerHeal(int healing)
     {
         GameManager.gameManager._playerHealth.HealUnit(healing);
+        _healthBar.SetHealth(GameManager.gameManager._playerHealth.Health);
     }
 }
