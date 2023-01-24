f11d6a0c01967f391a25496b0449d004b83b3fb2
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-01-24 18:32:48 -0500

    working on level design

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float followSpeed = 2f;
-    public float yOffSet = 1f;
     public Transform target;
+    public float yOffSet = 1f;
 
+    // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffSet, -10f);
+        Vector3 newPos = new Vector3 (target.position.x, target.position.y + yOffSet, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
